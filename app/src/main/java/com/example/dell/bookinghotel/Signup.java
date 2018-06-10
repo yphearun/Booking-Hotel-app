45c9c6910b3b70a613a8a6ad76d355e1e5a1de60
yphearun <yeanphearun7@gmail.com>
2018-06-10 09:34:40 +0700

    Booking Hotel

---
+++ app/src/main/java/com/example/dell/bookinghotel/Signup.java
@@ -0,0 +1,23 @@
+package com.example.dell.bookinghotel;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import java.util.zip.Inflater;
+
+
+public class Signup extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.signup, container, false);
+
+        return view;
+    }
+}
